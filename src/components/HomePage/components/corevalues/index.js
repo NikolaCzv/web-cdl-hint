cb1c39922cd56a0a73d566d28b4c20bed9f32fec
Rabbitosa <Rabbitosa2003@gmail.com>
2021-08-13 00:13:04 +0200

    core values

---
+++ src/components/HomePage/components/corevalues/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Content, ContentWrapper } from './style';
+
+const CoreValues = () => {
+    return (
+        <Container>
+            <h1>core values</h1>
+        </Container>
+    );
+};
+
+export default CoreValues;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import * as React from "react";
 import MainVideo from "../components/HomePage/components/video";
 import Applications from "../components/HomePage/components/applications";
+import CoreValues from "../components/HomePage/components/corevalues";
 
 
 const HomePage = () => (
   <div>
     <MainVideo />
     <Applications />
+    <CoreValues />
   </div>
 );
 
